0cb281868b6ec3dbb42052bb41096e2fc776ba0c
craftablescience <me@craftablescience.info>
2025-01-29 05:45:01 -0500

    feat: move sourcemods getter to its own files

--- src/NewModDialog.cpp
+++ src/NewModDialog.cpp
@@ -21,87 +21,9 @@
 #include <QProgressDialog>
 #include <QStandardPaths>
 
-#ifdef _WIN32
-	#include <memory>
-	#include <Windows.h>
-#else
-	#include <cstdlib>
-#endif
+#include "Steam.h"
 
 namespace {
-
-/// Copied from sourcepp
-[[nodiscard]] QString getSourceModsDirLocationHelper() {
-	std::filesystem::path steamLocation;
-	std::error_code ec;
-
-#ifdef _WIN32
-	{
-		// 16383 being the maximum length of a path
-		static constexpr DWORD STEAM_LOCATION_MAX_SIZE = 16383;
-		std::unique_ptr<char[]> steamLocationData{new char[STEAM_LOCATION_MAX_SIZE]};
-
-		HKEY steam;
-		if (RegOpenKeyExA(HKEY_LOCAL_MACHINE, R"(SOFTWARE\Valve\Steam)", 0, KEY_QUERY_VALUE | KEY_WOW64_32KEY, &steam) != ERROR_SUCCESS) {
-			return "";
-		}
-
-		DWORD steamLocationSize = STEAM_LOCATION_MAX_SIZE;
-		if (RegQueryValueExA(steam, "InstallPath", nullptr, nullptr, reinterpret_cast<LPBYTE>(steamLocationData.get()), &steamLocationSize) != ERROR_SUCCESS) {
-			return "";
-		}
-
-		RegCloseKey(steam);
-		steamLocation = steamLocationSize > 0 ? std::string(steamLocationData.get(), steamLocationSize - 1) : "";
-	}
-#else
-	{
-		std::filesystem::path home{std::getenv("HOME")};
-#ifdef __APPLE__
-		steamLocation = home / "Library" / "Application Support" / "Steam";
-#else
-		// Snap install takes priority, the .steam symlink may exist simultaneously with Snap installs
-		steamLocation = home / "snap" / "steam" / "common" / ".steam" / "steam";
-
-		if (!std::filesystem::exists(steamLocation, ec)) {
-			// Use the regular install path
-			steamLocation = home / ".steam" / "steam";
-		}
-#endif
-	}
-
-	if (!std::filesystem::exists(steamLocation, ec)) {
-		std::string location;
-		std::filesystem::path d{"cwd/steamclient64.dll"};
-		for (const auto& entry : std::filesystem::directory_iterator{"/proc/"}) {
-			if (std::filesystem::exists(entry / d, ec)) {
-				ec.clear();
-				const auto s = std::filesystem::read_symlink(entry.path() / "cwd", ec);
-				if (ec) {
-					continue;
-				}
-				location = s.string();
-				break;
-			}
-		}
-		if (location.empty()) {
-			return "";
-		} else {
-			steamLocation = location;
-		}
-	}
-#endif
-
-	if (auto sourceModPath = (steamLocation / "steamapps" / "sourcemods").string(); std::filesystem::exists(sourceModPath, ec)) {
-		return sourceModPath.c_str();
-	}
-	return "";
-}
-
-[[nodiscard]] const QString& getSourceModsDirLocation() {
-	static QString location = ::getSourceModsDirLocationHelper();
-	return location;
-}
 
 [[nodiscard]] QString join(const QStringList& list, const QString& separator) {
 	if (list.isEmpty()) {
@@ -217,7 +139,7 @@ NewModDialog::NewModDialog(QString gameRoot_, QString downloadURL_, QWidget* par
 		, gameRoot(std::move(gameRoot_))
 		, downloadURL(std::move(downloadURL_)) {
 	// Check for sourcemods
-	const bool knowsSourcemodsDirLocation = !::getSourceModsDirLocation().isEmpty();
+	const bool knowsSourcemodsDirLocation = !::getSourceModsDir().isEmpty();
 
 	// Window setup
 	this->setModal(true);
@@ -352,7 +274,7 @@ QString NewModDialog::getModInstallDirParent() const {
 	}
 	switch (selectedIndex) {
 		case 0:
-			return ::getSourceModsDirLocation();
+			return ::getSourceModsDir();
 		case 1:
 			return this->gameRoot;
 		default:

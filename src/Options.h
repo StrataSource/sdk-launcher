81c49fe5d0acc4a53a704b081a8ccf326114f699
craftablescience <me@craftablescience.info>
2025-12-30 19:44:33 -0800

    fix: checked original authoring tools, double-click to open is default

--- src/Options.h
+++ src/Options.h
@@ -6,7 +6,7 @@ constexpr std::string_view STR_RECENT_CONFIGS = "str_recent_configs";
 constexpr std::string_view STR_GAME_OVERRIDE = "str_game_override";
 
 constexpr std::string_view BOOL_SINGLE_CLICK_TO_RUN = "opt_single_click_to_run";
-constexpr bool BOOL_SINGLE_CLICK_TO_RUN_DEFAULT = true;
+constexpr bool BOOL_SINGLE_CLICK_TO_RUN_DEFAULT = false;
 
 namespace Options {
 

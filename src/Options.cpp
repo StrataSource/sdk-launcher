e16c16d5e90394bc14422ad00f6e6f305192bde7
craftablescience <me@craftablescience.info>
2025-12-30 19:37:17 -0800

    fix: more helpers, less duplication, more checks

--- src/Options.cpp
+++ src/Options.cpp
@@ -2,7 +2,15 @@
 
 #include "Config.h"
 
-QSettings& options() {
+QSettings& Options::get() {
 	static QSettings s{QString{"%1.ini"}.arg(PROJECT_TARGET_NAME.data()), QSettings::Format::IniFormat};
 	return s;
+}
+
+bool Options::contains(std::string_view key) {
+	return get().contains(key);
+}
+
+void Options::remove(std::string_view key) {
+	get().remove(key);
 }
